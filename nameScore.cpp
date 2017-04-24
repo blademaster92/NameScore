e4fb1ba94abd132b88545dd034d9ceac451db46b
atruelove <atruelove@users.noreply.github.com>
2017-04-24 15:21:04 -0500

    added class object initialization

--- nameScore.cpp
+++ nameScore.cpp
@@ -10,11 +10,11 @@ nameScore::nameScore() {
 	_pos = 0;
 }
 
-nameScore::nameScore(string nameText) {
+nameScore::nameScore(string nameText, int pos) {
 	_nameText = nameText;
 	_finalScore = 0;
 	_sum = 0;
-	_pos = 0;
+	_pos = pos;
 }
 
 void nameScore::setName(string nameText) {
@@ -30,26 +30,6 @@ int nameScore::getFinalScore() {
 }
 
 int nameScore::getSum() {
-	/*string name = this->getName();
-	int sum = 0;
-	char base = ' ';
-	char lowBase = '`';
-	char upperBase = '@';
-	for (int i = 0; i < name.length(); i++)
-	{
-	if (name[i] >= 'A' && name[i] <= 'Z')
-	{
-	base = upperBase;
-	}
-	else if (name[i] >= 'z' && name[i] <= 'z')
-	{
-	base = lowBase;
-	}
-	int value = name[i] - base;
-	//cout << value << endl;
-	sum += value;
-	//cout << sum << endl;
-	}/**/
 	setSum();
 	return _sum;
 }
@@ -70,4 +50,9 @@ void nameScore::setSum() {
 		int value = name[i] - base;
 		sum += value;
 	}	_sum = sum;
+}
+
+int nameScore::getPos()
+{
+	return _pos;
 }
